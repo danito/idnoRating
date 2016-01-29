6e8ecb8d3684d821578857c621bc4188b99a28d2
Daniel nix <daniel.nix@gmail.com>
2016-01-29 21:28:38 +0100

    Create Main.php

---
+++ Main.php
@@ -0,0 +1,15 @@
+<?php
+namespace IdnoPlugins\Rating {
+  class Main extends \Idno\Common\Plugin {
+    
+    function registerPages() {
+    	    // Add custom CSS
+	    \Idno\Core\site()->template()->extendTemplate('shell/head', 'rating/css');
+	        	    // Extend entity objects
+	    \Idno\Core\site()->template()->extendTemplate('entity/Entry', 'rating/entity/Entry');
+	    \Idno\Core\site()->template()->extendTemplate('entry/edit', 'rating/entry/edit');
+    }
+  
+  }
+}
+?>
